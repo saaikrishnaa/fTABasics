703b042e0dac54803744b53063b0512aa00d3c4a
saaikrishnaa <saikrishna.learning@gmail.com>
2022-05-05 21:40:21 +0530

    Adding changes for automated tests

--- src/test/java/com/fta/handson/tests/SauceDemoStandardUserLoginTest.java
+++ src/test/java/com/fta/handson/tests/SauceDemoStandardUserLoginTest.java
@@ -1,17 +1,40 @@
 package com.fta.handson.tests;
 
 import com.fta.handson.driver.DriverManager;
+import com.fta.handson.pages.LoginPage;
 import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class SauceDemoStandardUserLoginTest extends BaseTest{
 
-    @Test(description = "To test the login functionality in Sauce Labs application")
-    public void sauceLabsStandardUserLoginTest() throws InterruptedException {
-        DriverManager.getDriver().findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
-        DriverManager.getDriver().findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
-        DriverManager.getDriver().findElement(By.xpath("//input[@type='submit']")).click();
-        DriverManager.getDriver().findElement(By.xpath("//button[@id='react-burger-menu-btn']")).click();
-        DriverManager.getDriver().findElement(By.xpath("//a[@id='logout_sidebar_link']")).click();
+
+    @DataProvider(name = "provide")
+    public static Object[][] inputData() {
+        return new Object[][]{
+                {"standard_user","secret_sauce"}
+        };
+    }
+
+    @Test(description = "To test the login functionality in Sauce Labs application",dataProvider = "provide")
+    public void sauceLabsStandardUserLoginTest(String username, String password) throws InterruptedException {
+        LoginPage loginPage = new LoginPage();
+
+        DriverManager.getDriver().findElement(By.xpath("//input[@id='user-name']")).sendKeys(username); //Step1 - Normal way, without using Page Object Model
+        loginPage.setUsername("standard_user");//Step2 is better than Step1 - using Page Object Model
+
+        DriverManager.getDriver().findElement(By.xpath("//input[@id='password']")).sendKeys(password); //Step1 - Normal way, without using Page Object Model
+        loginPage.setPassword("secret_sauce");//Step2 is better than Step1 - using Page Object Model
+
+        DriverManager.getDriver().findElement(By.xpath("//input[@type='submit']")).click(); //Step1 - Normal way, without using Page Object Model
+        loginPage.clickLogin();//Step2 is better than Step1 - using Page Object Model
+
+        DriverManager.getDriver().findElement(By.xpath("//button[@id='react-burger-menu-btn']")).click(); //Step1 - Normal way, without using Page Object Model
+//        loginPage.clickHamburgerMenu();//Step2 is better than Step1 - using Page Object Model
+
+        DriverManager.getDriver().findElement(By.xpath("//a[@id='logout_sidebar_link']")).click(); //Step1 - Normal way, without using Page Object Model
+//        loginPage.clickLogout();//Step2 is better than Step1 - using Page Object Model
+
+        loginPage.loginToSauceDemoApplication(username,password);//Step3 is further better than Step2 - using the wrapper method
     }
 }
---
+++ src/test/java/com/fta/handson/tests/SauceLabsHomePageTest.java
@@ -0,0 +1,44 @@
+package com.fta.handson.tests;
+
+import com.fta.handson.pages.LoginPage;
+import com.fta.handson.pages.SauceLabsHomePage;
+import com.fta.handson.pages.components.LeftMenuComponent;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class SauceLabsHomePageTest extends BaseTest{
+
+    @DataProvider
+    public static Object[][] supplyData() {
+        return new Object[][]{
+                {"standard_user","secret_sauce"}
+        };
+    }
+
+
+    @Test(description = "To test the title of the Sauce Labs Home Page", dataProvider = "supplyData")
+    public void validateSauceLabsTitleTest(String username, String password){
+        LoginPage loginPage = new LoginPage();
+
+        loginPage.loginToSauceDemoApplication(username,password);
+
+        LeftMenuComponent leftMenuComponent = new LeftMenuComponent();
+
+//        //without using the method chaining
+//        leftMenuComponent.clickHamburgerMenu();
+//        leftMenuComponent.clickAbout();
+
+        //with method chaining
+        leftMenuComponent
+                .clickHamburgerMenu()
+                .clickAbout();
+
+        SauceLabsHomePage sauceLabsHomePage = new SauceLabsHomePage();
+        String actualValue = sauceLabsHomePage.getPageTitle();
+        String expectedValue = "";
+
+        Assert.assertEquals(actualValue, expectedValue);
+    }
+
+}
